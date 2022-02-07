9d62d5cce60628affa83c805b450a3f5f5b7ce74
SangYeonY <sa9798@naver.com>
2022-02-07 15:58:26 +0900

    day3 제약조건

---
+++ oracle/STUDENTS_202202032037.sql
@@ -0,0 +1,4 @@
+INSERT INTO STUDENTS (STUNO,STU_NAME,EMAIL,ADDRESS,ENTER_DAY,ETC) VALUES
+	 (1,'김모모','momo@gmail,com','서울시 강남구 역삼동',TIMESTAMP'2020-03-02 00:00:00.0',NULL),
+	 (2,'최나나','nana@naver.com','경기도 광주시 ',TIMESTAMP'2020-09-01 00:00:00.0',NULL),
+	 (3,'이다현','lee@korea.kr','부산광역시',TIMESTAMP'2021-03-01 00:00:00.0',NULL);
